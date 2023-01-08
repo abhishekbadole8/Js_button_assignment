c37822a2f2af218371234701fb92a244fec0ae7a
Abhishek Badole <92942952+abhishekbadole8@users.noreply.github.com>
2023-01-08 20:33:38 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+
+var i = 0
+document.querySelector(".displayNum").innerHTML = i
+
+
+function addOne() {
+    i++
+    document.querySelector(".displayNum").innerHTML = i
+}
+
+
+
+function subOne() {
+    i--
+    document.querySelector(".displayNum").innerHTML = i
+}
+
+
+
+
+function reset() {
+    i = 0
+    document.querySelector(".displayNum").innerHTML=i
+
+}
